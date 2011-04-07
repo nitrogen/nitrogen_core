a28c76147946e9ab0510a0354d3840b6cc6ef579
rkozmer <rado@kozmer.net>
2011-04-07 13:24:09 -0700

    New attribute 'placeholder'

--- src/elements/other/element_sortblock.erl
+++ src/elements/other/element_sortblock.erl
@@ -20,10 +20,10 @@ render_element(Record) ->
     end,
     ConnectWithGroups = groups_to_connect_with(Record#sortblock.connect_with_groups),
     GroupClasses = groups_to_classes(Record#sortblock.group),
-
+    Placeholder = Record#sortblock.placeholder,
     % Emit the javascript...
     Script = #script { 
-        script=wf:f("Nitrogen.$sortblock('~s', { handle: ~s, connectWith: [~s] }, '~s');", [Anchor, Handle, ConnectWithGroups, PostbackInfo])
+        script=wf:f("Nitrogen.$sortblock('~s', {handle: ~s, connectWith: [~s], placeholder: '~s'}, '~s');", [Anchor, Handle, ConnectWithGroups, Placeholder, PostbackInfo])
     },
     wf:wire(Script),
 
