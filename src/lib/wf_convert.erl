cbcbd4da2d2d331239abc837d46440700caf44dd
Jesse Gumm <sigmastar@gmail.com>
2011-11-15 03:40:09 -0600

    wf:to_list/1 (with float passed) will display only the necessary number of decimal places. (ie 1.0 will display as "1")

--- src/lib/wf_convert.erl
+++ src/lib/wf_convert.erl
@@ -35,7 +35,11 @@ to_list(A) -> inner_to_list(A).
 inner_to_list(A) when is_atom(A) -> atom_to_list(A);
 inner_to_list(B) when is_binary(B) -> binary_to_list(B);
 inner_to_list(I) when is_integer(I) -> integer_to_list(I);
-inner_to_list(F) when is_float(F) -> nitro_mochinum:digits(F);
+inner_to_list(F) when is_float(F) -> 
+	case F == round(F) of
+		true -> inner_to_list(round(F));
+		false -> nitro_mochinum:digits(F)
+	end;
 inner_to_list(L) when is_list(L) -> L.
 
 to_atom(A) when is_atom(A) -> A;
@@ -83,6 +87,7 @@ to_string_list([H|T], Acc) ->
 
 html_encode(L,Fun) when is_function(Fun) -> Fun(L);
 
+html_encode(L,EncType) when is_atom(L) -> html_encode(wf:to_list(L),EncType);
 html_encode(L,EncType) when is_integer(L) -> html_encode(integer_to_list(L),EncType);
 html_encode(L,EncType) when is_float(L) -> html_encode(nitro_mochinum:digits(L),EncType);
 
