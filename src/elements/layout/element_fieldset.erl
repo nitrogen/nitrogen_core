82411fcc7882c872f056b3d57a26bf66d4d9951a
Jesse Gumm <sigmastar@gmail.com>
2012-03-04 23:47:48 -0600

    Add fieldset element and associated docs

---
+++ src/elements/layout/element_fieldset.erl
@@ -0,0 +1,23 @@
+-module (element_fieldset).
+-compile(export_all).
+-include_lib("wf.hrl").
+
+reflect() -> record_info(fields, fieldset).
+
+render_element(R = #fieldset{}) ->
+	LegendBody = [
+		wf:html_encode(R#fieldset.legend_text,R#fieldset.legend_html_encode),
+		R#fieldset.legend_body
+	],
+	Body = [
+		wf_tags:emit_tag('legend',LegendBody, [
+			{class, ["legend"]}
+		]),
+		wf:html_encode(R#fieldset.text,R#fieldset.html_encode),
+		R#fieldset.body
+	],
+
+	wf_tags:emit_tag('fieldset',Body, [
+		{class, ["fieldset", R#fieldset.class]},
+		{style, R#fieldset.style}
+	]).
