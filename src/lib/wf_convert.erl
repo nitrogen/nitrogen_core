e67baac793d614707e7a8b17d7a71129bd29f732
Jesse Gumm <sigmastar@gmail.com>
2012-05-16 13:56:57 -0500

    Fix so wf:wire better supports binaries.

--- src/lib/wf_convert.erl
+++ src/lib/wf_convert.erl
@@ -155,7 +155,7 @@ url_decode(S) -> unquote(S).
 %%% ESCAPE JAVASCRIPT %%%
 
 js_escape(undefined) -> [];
-js_escape(Value) when is_list(Value) -> binary_to_list(js_escape(list_to_binary(lists:flatten(Value))));
+js_escape(Value) when is_list(Value) -> binary_to_list(js_escape(iolist_to_binary(Value)));
 js_escape(Value) -> js_escape(Value, <<>>).
 js_escape(<<"\\", Rest/binary>>, Acc) -> js_escape(Rest, <<Acc/binary, "\\\\">>);
 js_escape(<<"\r", Rest/binary>>, Acc) -> js_escape(Rest, <<Acc/binary, "\\r">>);
