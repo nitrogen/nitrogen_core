f5b5a03189172137cdc62ec7513ec5e252273562
Steffen Panning <steffen.panning@googlemail.com>
2012-06-03 17:37:30 +0200

    Added documentation doc/org-mode/elements.org doc/org-mode/elements/recaptcha.org
    bugfixing
    src/elements/other/element_recaptcha.erl

--- src/elements/other/element_recaptcha.erl
+++ src/elements/other/element_recaptcha.erl
@@ -9,15 +9,16 @@
 reflect() -> record_info(fields, recaptcha).
 
 render_element(#recaptcha{id=ID, class=Cl, delegate=Delegate,
-               fail_body=FB, captcha_opts=COPts, btn_label=BtnLabel})->
+               fail_body=FB, captcha_opts=COPts, 
+               btn_label=BtnLabel, btn_id=BtnId})->
     % Since the recaptcha is rendered outside of the nitrogen framework
     % it doesn't play along when it comes to data exchange.
     % This hack transfers the alien content into #hidden elements before
     % postback. The content is then accessible via wf:q()
-    wf:wire(submit_recaptcha, #event{type=click, actions=[
+    wf:wire(BtnId, #event{type=click, actions=[
         % transfer happens here
         #script{script="Nitrogen.$from_alien('recaptcha_challenge_field')"},
-        #script {script="Nitrogen.$from_alien('recaptcha_response_field')" }]}),
+        #script{script="Nitrogen.$from_alien('recaptcha_response_field')" }]}),
     Src = lists:flatten([challenge_url(), "?k=", public_key()]),
     Postback={eval_recaptcha, ID, Delegate, FB},
     #panel{id=ID, class=Cl, style="width:315px",
@@ -31,10 +32,20 @@ render_element(#recaptcha{id=ID, class=Cl, delegate=Delegate,
                 #panel{id=fail_msg,
                        style="visibility:hidden;float:left",
                        body=FB},
-                #button{id=submit_recaptcha, style="float:right",
+                #button{id=BtnId, style="float:right",
                         text=BtnLabel,
                         postback=Postback, delegate=?MODULE}]},
             #p{style="clear:both"}]}.
+
+event({eval_recaptcha, CaptchaID, Delegate, Fail}) ->
+    Challenge = wf:q(recaptcha_challenge_field),
+    Response = wf:q(recaptcha_response_field),
+    Evaled = evaluate_captcha(CaptchaID, Challenge, Response),
+    case Evaled of
+        ok     -> process_callback({recaptcha, Evaled}, Delegate);
+        _else  -> wf:wire("Recaptcha.reload()"),
+		  replace_error_message(Fail)
+    end.
 
 %% internal
 get_config_fields(Field)->
@@ -67,23 +78,24 @@ ip_to_str(IPTuple) ->
 
 mk_post_request(Url, Params)->
     MkField = fun(K, V) ->
-        lists:flatten([atom_to_list(K), "=", http_uri:encode(V)])
-    end,
+                      lists:flatten([atom_to_list(K), "=",
+                                     http_uri:encode(V)])
+              end,
     Fields = [MkField(K,V) || {K,V} <- Params],
    {Url, [], "application/x-www-form-urlencoded", string:join(Fields,"&")}.
 
 render_options(OptProps, Defaults) ->
     ValToStr = fun(V) ->
-                 case V of
-                   V when is_list(V) ->
-                       ": '" ++ V ++ "'";
-                   V when V == null ->
-                     ": null";
-                   V when is_atom(V) ->
-                      ": '" ++  atom_to_list(V) ++ "'";
-                  (V) when is_integer(V)->
-                      ": " ++ integer_to_list(V)
-                 end
+                       case V of
+                           V when is_list(V) ->
+                               ": '" ++ V ++ "'";
+                           V when V == null ->
+                               ": null";
+                           V when is_atom(V) ->
+                               ": '" ++  atom_to_list(V) ++ "'";
+                           (V) when is_integer(V)->
+                               ": " ++ integer_to_list(V)
+                       end
                end,
     SortedOpts = lists:keysort(1, OptProps),
     SortedDef  = lists:keysort(1, Defaults),
@@ -101,37 +113,39 @@ default_opts() ->
      {custom_theme_widget, null}, %not supported
      {tabindex,            0}].
 
-evaluate_captcha(CaptchaID, Challenge, Response, Fail) ->
+evaluate_captcha(CaptchaID, Challenge, Response) ->
     Url = verify_url(),
     Re = httpc:request(post,
-        mk_post_request(Url, [{privatekey, private_key()},
+                       mk_post_request(Url, [{privatekey, private_key()},
                               {challenge,  Challenge},
                               {response,   Response},
                               {remoteip, ip_to_str(wf_context:peer_ip())}]),
         [],[]),
     case Re of
         {ok, {{_Version, 200, _Reason}, _Hdr, Body}} ->
-            parse_response(CaptchaID, Body, Fail);
+            parse_response(CaptchaID, Body);
         {error, Reason}                              ->
             {error, Reason}
     end.
 
-parse_response(CaptchaID, Body, Fail) ->
+replace_error_message(Msg) ->
+    wf:replace(fail_msg,
+	       #panel{id=fail_msg,
+		      style="visibility:visible;float:left",
+		      body=Msg}).
+
+parse_response(CaptchaID, Body) ->
     case string:tokens(Body, "\n") of
-        ["true", "success"] -> wf:remove(CaptchaID),
-                               ok;
-        ["false", Error]    -> wf:wire("Recaptcha.reload()"),
-                               wf:replace(fail_msg,
-                                  #panel{id=fail_msg,
-                                         style="visibility:visible;float:left",
-                                         body=Fail}),
-                               {error, Error};
+        ["true", "success"] -> ok;
+        ["false", Error]    -> {error, Error};
         ERROR               -> {unknown, ERROR}
     end.
 
-event({eval_recaptcha, CaptchaID, Delegate, Fail}) ->
-    Challenge = wf:q(recaptcha_challenge_field),
-    Response = wf:q(recaptcha_response_field),
-    Evaled = evaluate_captcha(CaptchaID, Challenge, Response, Fail),
+process_callback(Event, Delegate) ->
     Module = wf:coalesce([Delegate, wf:page_module()]),
-    Module:event({recaptcha, Evaled}).
+    case Module:event(Event) of
+        ok          -> ok;
+        error       -> wf:wire("Recaptcha.reload()");
+        {error,Msg} -> wf:wire("Recaptcha.reload()"),
+		       replace_error_message(Msg)
+    end.
