7567102c0e305cfc56245913aa1ab0ce90308dd8
Milan Svoboda <msvoboda@ra.rockwell.com>
2012-08-09 07:53:21 +0200

    add disabled option to hidden element

--- src/elements/forms/element_hidden.erl
+++ src/elements/forms/element_hidden.erl
@@ -9,11 +9,18 @@
 reflect() -> record_info(fields, hidden).
 
 render_element(Record) -> 
+
     Value = wf:html_encode(Record#hidden.text, Record#hidden.html_encode),
+
+    Disabled = case Record#hidden.disabled of
+        true -> [{disabled}];
+        false -> []
+    end,
+
     wf_tags:emit_tag(input, [
         {id, Record#hidden.html_id},
         {class, Record#hidden.class},
         {type, hidden},
         {name, Record#hidden.html_name},
         {value, Value}
-    ]).
+    ] ++ Disabled).
