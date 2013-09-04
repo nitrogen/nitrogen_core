af32d8ef1dfd2007f0b43a56e7e3a0bf422bb7e4
Boris Resnick <resnick@ians.aero>
2013-09-04 05:07:02 -0400

    Using 'nitro_mochinum' instead of 'mochinum'

--- src/lib/nitro_mochijson2.erl
+++ src/lib/nitro_mochijson2.erl
@@ -140,7 +140,7 @@ json_encode(null, _State) ->
 json_encode(I, _State) when is_integer(I) ->
     integer_to_list(I);
 json_encode(F, _State) when is_float(F) ->
-    mochinum:digits(F);
+    nitro_mochinum:digits(F);
 json_encode(S, State) when is_binary(S); is_atom(S) ->
     json_encode_string(S, State);
 json_encode([{K, _}|_] = Props, State) when (K =/= struct andalso
