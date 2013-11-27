37564c67031321697ebae559769de72a474d9998
Jesse Gumm <sigmastar@gmail.com>
2013-11-26 22:20:41 -0600

    Set up stubs for websockets. Use 'nitrogen' as callout module.

--- src/nitrogen.erl
+++ src/nitrogen.erl
@@ -3,12 +3,25 @@
 % Copyright (c) 2008-2010 Rusty Klophaus
 % See MIT-LICENSE for licensing information.
 
--module (nitrogen).
--export ([
-	init_request/2,
-    init_request/1,
-	handler/2,
-	run/0]).
+-module(nitrogen).
+-behaviour(simple_bridge_callout).
+-export([
+        init_request/2,
+        init_request/1,
+        handler/2,
+        run/0
+    ]).
+
+%% Simple Bridge Callout functions
+-export([
+        run/1,
+        ws_init/1,
+        ws_message/2,
+        ws_info/2,
+        ws_terminate/2
+    ]).
+    
+
 
 %% init_request/2 kept for backwards compatibility, but is no longer needed
 init_request(Bridge, _) ->
@@ -22,3 +35,21 @@ handler(Module, Config) ->
 
 run() -> 
     wf_core:run().
+
+
+run(Bridge) ->
+    init_request(Bridge),
+    nitrogen_callout:run().
+
+ws_init(Bridge) ->
+    init_request(Bridge),
+    ok.
+
+ws_message({text, _Msg}, _Bridge) ->
+    noreply.
+
+ws_info(_Msg, _Bridge) ->
+    noreply.
+
+ws_terminate(_Reason, _Bridge) ->
+    close.
