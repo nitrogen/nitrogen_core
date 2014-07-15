8ff5c3d2ad8ba00b954698b0c5f159218dadd68d
Jesse Gumm <gumm@sigma-star.com>
2014-07-14 19:59:38 -0500

    Add to copyright on nitrogen.erl (such essential)

--- src/nitrogen.erl
+++ src/nitrogen.erl
@@ -1,6 +1,7 @@
 % vim: sw=4 ts=4 et ft=erlang
 % Nitrogen Web Framework for Erlang
 % Copyright (c) 2008-2010 Rusty Klophaus
+% Copyright (c) 2013-2014 Jesse Gumm
 % See MIT-LICENSE for licensing information.
 
 -module(nitrogen).
