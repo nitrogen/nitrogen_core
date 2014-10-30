1eccc406379899541355f25588ffb26924dee8bf
Jesse Gumm <gumm@sigma-star.com>
2014-10-30 17:11:30 -0500

    Reorganize docs a bit

--- doc/org-mode/update_headers.sh
+++ doc/org-mode/update_headers.sh
@@ -4,4 +4,5 @@
 ./replace_header.pl org-headers/elements_header.org.src elements/*.org
 ./replace_header.pl org-headers/validators_header.org.src validators/*.org
 ./replace_header.pl org-headers/handlers_header.org.src handlers/*.org
+./replace_header.pl org-headers/advanced_header.org.src advanced/*.org
 
