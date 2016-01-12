fd8f94ee75a610c7bd7dcb12542f08c88f32c7e0
Jesse Gumm <gumm@sigma-star.com>
2016-01-12 17:26:25 -0600

    Refactor wf_render_elements to pass dialyzer

--- include/wf.hrl
+++ include/wf.hrl
@@ -53,6 +53,8 @@
 -type module_function()     :: {atom(), atom()}.
 -type encoding_function()   :: module_function() | fun((iolist()) -> iolist()).
 -type encoding()            :: none | unicode | auto | encoding_function().
+-type context_data()        :: iolist() | {file, Filename :: path()}
+                                | {stream, Size :: integer(), fun()}.
 -type context_type()        :: first_request | postback_request | static_file | postback_websocket.
 %%% CONTEXT %%%
 
@@ -95,7 +97,7 @@
     type                    :: context_type(),
     bridge                  :: simple_bridge:bridge(),
     anchor=undefined        :: id(), 
-    data=[]                 :: iolist(),
+    data=[]                 :: context_data(),
     encoding=auto           :: encoding(),
     action_queue=undefined  :: wf_action_queue:action_queue() | undefined,
     % These are all serialized, sent to the browser
