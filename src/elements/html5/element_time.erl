5caf406913fcb75af6382396821d68d9763e5dfb
Stuart Thackray <stuart.thackray@gmail.com>
2016-02-01 10:34:47 +0200

    remvoed  "Change to the way time is outputed"
    This reverts commit 6b0556861308bad2f8ebb60ce81074e2185dd05a.

--- src/elements/html5/element_time.erl
+++ src/elements/html5/element_time.erl
@@ -15,8 +15,7 @@ reflect() -> record_info(fields, time).
 -spec render_element(#time{}) -> body().
 render_element(Record) ->
     Text = wf:html_encode(Record#time.text, Record#time.html_encode),
-    wf_tags:emit_tag(input, [Text, Record#time.body], [
-        {type, time},
+    wf_tags:emit_tag(time, [Text, Record#time.body], [
         {id, Record#time.html_id},
         {class, ["time", Record#time.class]},
         {title, Record#time.title},
