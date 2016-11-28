646e38a5d72ecf21e3d3031689e3ce83b55ab4e8
Jesse Gumm <gumm@sigma-star.com>
2016-11-28 07:50:42 -0600

    Remove enforcement rules from html5_header

--- src/elements/html5/element_html5_header.erl
+++ src/elements/html5/element_html5_header.erl
@@ -14,27 +14,11 @@ reflect() -> record_info(fields, html5_header).
 
 -spec render_element(#html5_header{}) -> body().
 render_element(Record) ->
-    CheckHeaderOrFooter =   fun (X) ->
-                                if
-                                    is_record(X, html5_header) ->
-                                        true;
-                                    is_record(X, html5_footer) ->
-                                        true;
-                                    true ->
-                                        false
-                                end
-                            end,
-    Y = lists:any(CheckHeaderOrFooter, Record#html5_header.body),
-    if
-        Y ->
-            "<b style=\"color: red;\">html5_header cannot have another html5_header or html5_footer as child element</b>";
-        true ->
-            wf_tags:emit_tag('header', Record#html5_header.body, [
-                {id, Record#html5_header.html_id},
-                {class, ["html5_header", Record#html5_header.class]},
-                {title, Record#html5_header.title},
-                {style, Record#html5_header.style},
-                {role, Record#html5_header.role},
-                {data_fields, Record#html5_header.data_fields}
-            ])
-    end.
+    wf_tags:emit_tag('header', Record#html5_header.body, [
+        {id, Record#html5_header.html_id},
+        {class, ["html5_header", Record#html5_header.class]},
+        {title, Record#html5_header.title},
+        {style, Record#html5_header.style},
+        {role, Record#html5_header.role},
+        {data_fields, Record#html5_header.data_fields}
+    ]).
