0c955d0595495fd46713952457232439bcb125da
Jesse Gumm <gumm@sigma-star.com>
2016-11-28 07:49:32 -0600

    Remove the unnecessary enforcement rules from html5_footer

--- src/elements/html5/element_html5_footer.erl
+++ src/elements/html5/element_html5_footer.erl
@@ -14,27 +14,11 @@ reflect() -> record_info(fields, html5_footer).
 
 -spec render_element(#html5_footer{}) -> body().
 render_element(Record ) ->
-    CheckHeaderOrFooter =   fun (X) ->
-                                if
-                                    is_record(X, html5_header) ->
-                                        true;
-                                    is_record(X, html5_footer) ->
-                                        true;
-                                    true ->
-                                        false
-                                end
-                            end,
-    Y = lists:any(CheckHeaderOrFooter, Record#html5_footer.body),
-    if
-        Y ->
-            "<b style=\"color: red;\">html5_footer cannot have another html5_header or html5_footer as child element</b>";
-        true ->
-            wf_tags:emit_tag('footer', Record#html5_footer.body, [
-                {id, Record#html5_footer.html_id},
-                {class, ["html5_footer", Record#html5_footer.class]},
-                {title, Record#html5_footer.title},
-                {style, Record#html5_footer.style},
-                {role, Record#html5_footer.role},
-                {data_fields, Record#html5_footer.data_fields}
-            ])
-    end.
+    wf_tags:emit_tag('footer', Record#html5_footer.body, [
+        {id, Record#html5_footer.html_id},
+        {class, ["html5_footer", Record#html5_footer.class]},
+        {title, Record#html5_footer.title},
+        {style, Record#html5_footer.style},
+        {role, Record#html5_footer.role},
+        {data_fields, Record#html5_footer.data_fields}
+    ]).
