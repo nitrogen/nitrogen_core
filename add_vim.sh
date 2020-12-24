dc8baa24a864099d13c0a15964b502620844205f
Jesse Gumm <gumm@sigma-star.com>
2020-12-23 20:06:18 -0600

    Remove org from vim script

--- add_vim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-vim4erl=`cat utils/vim-headers/vim4.erl.src`;
-vim3org=`cat utils/vim-headers/vim3.org.src`;
-
-find doc/org-mode -name "*.org" | xargs grep -L ":vim" | xargs sed -i "1i$vim3org"
-find src/vim -name "*.erl" | xargs grep -L ":vim" | xargs sed -i "1i$vim4erl"
