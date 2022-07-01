0c86cd145d7017539ce02d1e80621cab118bc9d9
Jesse Gumm <gumm@sigma-star.com>
2022-07-01 14:37:14 -0500

    Fix bug in #if_value

--- src/actions/action_if_value.erl
+++ src/actions/action_if_value.erl
@@ -14,9 +14,11 @@ render_action(#if_value{target=Target, map=Map, value=undefined, else=Else}) ->
         wf:f(<<"switch(objs('~s').val()) {">>, [Target]),
             lists:map(fun({Value0, Actions}) ->
                 Value = wf:js_escape(wf:to_list(Value0)),
-                wf:f(<<"case('~s'): ">>,[Value]),
-                Actions,
-                <<"break;">>
+                [
+                    wf:f(<<"case('~s'): ">>,[Value]),
+                    Actions,
+                    <<"break;">>
+                ]
             end, Map),
             <<"default: ">>,
             Else,
