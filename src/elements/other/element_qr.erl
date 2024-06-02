0f4d1082dca020b4b016963dd8c015b6f780b212
Jesse Gumm <gumm@sigma-star.com>
2024-06-02 12:03:13 -0500

    Cherry-pick QR code implementation from 3.x branch

--- src/elements/other/element_qr.erl
+++ src/elements/other/element_qr.erl
@@ -1,46 +1,34 @@
 % vim: sw=4 ts=4 et ft=erlang
 % Nitrogen Web Framework for Erlang
-% Copyright (c) 2008-2010 Rusty Klophaus
+% Copyright (c) 2024 Jesse Gumm
 % See MIT-LICENSE for licensing information.
 
 -module (element_qr).
 -include("wf.hrl").
 -export([
 	reflect/0,
-	transform_element/1
+	render_element/1
 ]).
 
 -spec reflect() -> [atom()].
 reflect() -> record_info(fields, qr).
 
--spec transform_element(#qr{}) -> body().
-transform_element(#qr{data=Empty} = QR) when Empty =:= undefined;
+-spec render_element(#qr{}) -> body().
+render_element(#qr{data=Empty} = QR) when Empty =:= undefined;
                                           Empty =:= <<"">>;
                                           Empty =:= "" ->
-    transform_element(QR#qr{data=wf:url()});
-transform_element(#qr{data=Data, size=Size, class=Class, id=Id, title=Title}) ->
-    BSize = wf:to_binary(Size),
-    Cht = <<"qr">>,
-    Chs = <<BSize/binary,"x",BSize/binary>>,
-    Choe = "UTF-8",
-
-    %% Not sure what this is exactly, but it's recommended to stay at H. Feel
-    %% free to examine if you wish. From
-    %% http://www.webmaster-source.com/2010/10/11/generate-qr-codes-on-the-fly-with-the-google-chart-api/
-    Chld = "H",
-    Chl = Data,
-
-    Path = "http://chart.apis.google.com/chart?",
-    QS = wf:to_qs([
-        {cht, Cht},
-        {chs, Chs},
-        {choe, Choe},
-        {chld, Chld},
-        {chl, Chl}
-    ]),
-    #image{
-       id=Id,
-       class=Class,
-       title=Title,
-       image=[Path,QS]
-    }.
+    render_element(QR#qr{data=wf:url()});
+render_element(#qr{data=Data, size=Size, class=Class, title=Title, data_fields=DataFields, style=Style}) ->
+    {SvgAttrs, Body} = nqr_svg:tag(Data),
+    XMLNS = "http://www.w3.org/2000/svg",
+    SvgAttrs2 = [{xmlns, XMLNS} | SvgAttrs],
+    Attrs = [
+        {class, Class},
+        {title, Title},
+        {data_fields, DataFields},
+        {width, Size},
+        {height, Size},
+        {style, Style}
+    ],
+    Attrs2 = SvgAttrs2 ++ Attrs,
+    wf_tags:emit_tag(svg, Body, Attrs2).
