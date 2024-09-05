7383ab58eee2d273a0467be09f03117ac0e3ad1a
Jesse Gumm <gumm@sigma-star.com>
2024-09-04 20:32:15 -0500

    Add new minify script

--- priv/www/minify.sh
+++ priv/www/minify.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-## On ubuntu/debian machines, the package to install is called yui-compressor:
-##   $ sudo apt-get install yui-compressor
+## On ubuntu/debian machines, this will use the node/nom package uglify-js
+##   $ https://www.tecmint.com/minify-css-and-js-files-linux/
 ## 
 
-min="yui-compressor"
+min="uglifyjs -c"
 
 $min -o bert.min.js bert.js
 $min -o nitrogen.min.js nitrogen.js
-cat jquery-ui.js jquery.ui.touch-punch.js | $min --type js -o jquery-ui.min.js
-cat fileupload/jquery.ui.widget.js fileupload/jquery.iframe-transport.js fileupload/jquery.fileupload.js | $min --type js -o jquery.fileupload.min.js
+cat jquery-ui.js jquery.ui.touch-punch.js | $min -o jquery-ui.min.js
+cat fileupload/jquery.ui.widget.js fileupload/jquery.iframe-transport.js fileupload/jquery.fileupload.js | $min -o jquery.fileupload.min.js
 $min -o jquery.sparkline.min.js jquery.sparkline.js
 $min -o livevalidation.min.js livevalidation.js
-$min -o jquery.mobile.min.js jquery.mobile.js
+$min -o nitro_livevalidation.min.js nitro_livevalidation.js
 
