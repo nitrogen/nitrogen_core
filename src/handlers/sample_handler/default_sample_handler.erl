5769bff373c630446dd2bb4b377b1dc7338b0039
Jesse Gumm <gumm@sigma-star.com>
2025-04-01 20:18:14 -0500

    Add handler sample/stub in src/handlers/sample_handler

---
+++ src/handlers/sample_handler/default_sample_handler.erl
@@ -0,0 +1,27 @@
+% vim: sw=4 ts=4 et ft=erlang
+% Nitrogen Web Framework for Erlang
+% Copyright (c) 2025 Jesse Gumm
+% See MIT-LICENSE for licensing information.
+
+-module(default_sample_handler).
+-include("wf.hrl").
+-behaviour(sample_handler).
+-export ([
+    init/2,
+    finish/2,
+    something_that_changes_handler_state/4,
+    something_that_does_not_change_handler_state/3
+]).
+
+init(_Config, State) ->
+    {ok, State}.
+
+finish(_Config, _State) ->
+    {ok, []}.
+
+something_that_changes_handler_state(Arg1, Arg2, Config, State) ->
+    RetVal = {Arg1, Arg2}, %% just doing something arbitrary with the passed args
+    {ok, RetVal, State}.
+
+something_that_does_not_change_handler_state(Arg1, Config, State) ->
+    ok.
---
+++ src/handlers/sample_handler/sample_handler.erl
@@ -0,0 +1,41 @@
+% vim: sw=4 ts=4 et ft=erlang
+% Nitrogen Web Framework for Erlang
+% Copyright (c) 2025 Jesse Gumm
+% See MIT-LICENSE for licensing information.
+
+-module(sample_handler).
+-include("wf.hrl").
+-export ([
+    something_that_changes_handler_state/2
+    something_that_does_not_change_handler_state/1
+]).
+
+-callback init(         handler_config(),
+                        handler_state()) -> {ok, handler_state()}.
+-callback finish(       handler_config(),
+                        handler_state()) -> {ok, handler_state()}.
+-callback something_that_changes_handler_state(Arg1 :: term(),
+                        Arg2 :: term(),
+                        handler_config(),
+                        handler_state()) -> {ok, RetVal :: term(), handler_state()}.
+-callback something_that_does_not_change_handler_state(Arg1 :: term(),
+                        handler_config(),
+                        handler_state()) -> term().
+
+%% behaviors that return a potentially new handler state will have to return either:
+%% {ok, State}
+%% {ok, RetVal, State}
+%% {ok, RetVal1, RetVal2, State}
+%% The return value from wf_handler:call will then be (respective to the above)
+%% ok
+%% {ok, RetVal}
+%% {ok, RetVal1, RetVal2}
+
+-spec something_that_changes_handler_state(Arg1 :: term(), Arg2 :: term()) -> {ok, term()}.
+something_that_changes_handler_state(Arg1, Arg2) ->
+    {ok, _Value} = wf_handler:call(x_handler, something_that_changes_handler_state, [Arg1, Arg2]).
+
+
+%% behavior functions that don't change handler state just return a value.
+something_that_does_not_change_handler_state(Arg1) ->
+    _Value = wf_handler:call_readonly(x_handler, something_that_does_not_change_handler_state, [Arg1]).
