b4b0dd028106b67f904c41143b09d6061960b105
Jesse Gumm <gumm@sigma-star.com>
2025-06-15 12:28:22 -0500

    Fix boilerplate on #hide action

--- src/actions/action_hide.erl
+++ src/actions/action_hide.erl
@@ -3,9 +3,9 @@
 % Copyright (c) 2008-2010 Rusty Klophaus
 % See MIT-LICENSE for licensing information.
 
--module (action_hide).
--include_lib ("wf.hrl").
--compile(export_all).
+-module(action_hide).
+-include("wf.hrl").
+-export([render_action/1]).
 
 render_action(Record) ->
     #jquery_effect {
