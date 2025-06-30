49ea5ad01eb03d1c28632f4a603754c7e137f13e
Jesse Gumm <gumm@sigma-star.com>
2025-06-30 11:17:01 -0500

    Add secret_handler comment

--- src/handlers/secret/default_secret_handler.erl
+++ src/handlers/secret/default_secret_handler.erl
@@ -2,6 +2,9 @@
 % Nitrogen Web Framework for Erlang
 % Copyright (c) 2025 Jesse Gumm
 % See MIT-LICENSE for licensing information.
+%
+% This handler is powered by the nitro_secret dependency:
+% https://github.com/nitrogen/nitro_secret
 
 -module(default_secret_handler).
 -include("wf.hrl").
@@ -18,5 +21,5 @@ init(_Config, State) ->
 finish(_Config, _State) ->
     {ok, []}.
 
-get_value(Key, Default, _Config, State) ->
+get_value(Key, Default, _Config, _State) ->
     nitro_secret:get(Key, Default).
